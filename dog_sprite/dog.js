50056122f81def6ca69f810cb142e71e3a1897a7
Paulo Vieira <paulo92vieira@gmail.com>
2024-09-03 13:11:20 +0100

    fixed

--- dog_sprite/dog.js
+++ dog_sprite/dog.js
@@ -3,18 +3,21 @@ let isJumping = false;
 
 function startDogAnimation() {
   dog.style.display = "block";
+  dog.style.left = "120px"; 
 
-  dog.addEventListener("animationend", (event) => {
-    if (event.animationName === "dogMove" && !isJumping) {
+  setTimeout(() => {
+    if (!isJumping) {
       stopWalkingAndPrepareJump();
     }
-  });
+  }, 5000); 
 }
 
 function stopWalkingAndPrepareJump() {
+  if (isJumping) return; 
   isJumping = true;
+
   // Stop all animations but keep the current position
-  let currentLeft = window.getComputedStyle(dog).left;
+  let currentLeft = window.getComputedStyle(dog).getPropertyValue("left");
   dog.style.animation = "none";
   dog.style.left = currentLeft; // Maintain the horizontal position
 
@@ -30,17 +33,17 @@ function stopWalkingAndPrepareJump() {
 
 function jump() {
   let currentLeft = dog.style.left;
-
   dog.style.transition = "bottom 0.3s ease-out";
   dog.style.bottom = "550px";
+
   setTimeout(() => {
     dog.style.transition = "bottom 0.3s ease-in";
     dog.style.bottom = "400px";
-
     dog.style.left = currentLeft;
 
     setTimeout(() => {
       dog.style.display = "none";
+      isJumping = false;
     }, 300);
   }, 500);
 }
