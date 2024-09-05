53bd01b9f82d0495cb1fd41ae79fd01db735a436
Paulo Vieira <paulo92vieira@gmail.com>
2024-09-05 10:04:05 +0100

    fixed

--- dog_sprite/dog.js
+++ dog_sprite/dog.js
@@ -1,19 +1,51 @@
 const dog = document.getElementById("dog");
 let isJumping = false;
+const mediaQueries = {
+  mobile: window.matchMedia("(max-width: 430px) and (max-height: 932px)"),
+  ipad: window.matchMedia("(max-width: 1024px) and (max-height: 1366px)"),
+};
+function getDeviceType() {
+  if (mediaQueries.mobile.matches) return "mobile";
+  if (mediaQueries.ipad.matches) return "ipad";
+  return "desktop";
+}
+function adjustAnimationForDevice() {
+  const deviceType = getDeviceType();
+  let scale, bottom, duration;
+  switch (deviceType) {
+    case "mobile":
+      scale = 3;
+      bottom = 100;
+      duration = 3;
+      break;
+    case "ipad":
+      scale = 4;
+      bottom = 150;
+      duration = 4;
+      break;
+    default:
+      scale = 4;
+      bottom = 200;
+      duration = 5;
+  }
+  dog.style.scale = scale;
+  dog.style.bottom = `${bottom}px`;
+  return duration;
+}
 
 /**************************DOG ANIMATION***************************** */
 
 function startDogAnimation() {
+  const duration = adjustAnimationForDevice();
   dog.style.display = "block";
-  dog.style.left = "120px";
-  dog.style.animation =
-    "dogWalk 0.8s steps(1) infinite, dogMove 5s linear forwards";
-
+  dog.style.left = "10%";
+  dog.style.bottom = "80%";
+  dog.style.animation = `dogWalk 0.8s steps(1) infinite, dogMove ${duration}s linear forwards`;
   setTimeout(() => {
     if (!isJumping) {
       stopWalkingAndPrepareJump();
     }
-  }, 5000);
+  }, duration * 1000);
 }
 
 /**************************DOG WALK AND STOP TO JUMP***************************** */
@@ -21,32 +53,38 @@ function startDogAnimation() {
 function stopWalkingAndPrepareJump() {
   if (isJumping) return;
   isJumping = true;
-
-  // Stop all animations but keep the current position
   let currentLeft = window.getComputedStyle(dog).getPropertyValue("left");
   dog.style.animation = "none";
-  dog.style.left = currentLeft; // Maintain the horizontal position
-
-  // Change to first sprite of second row
+  dog.style.left = currentLeft;
   dog.style.backgroundPosition = "0px -58px";
-
   setTimeout(() => {
-    // Change to second sprite of second row for jump
     dog.style.backgroundPosition = "-60px -58px";
     jump();
   }, 500);
 }
-
 function jump() {
+  const deviceType = getDeviceType();
+  let jumpHeight, initialBottom;
+  switch (deviceType) {
+    case "mobile":
+      jumpHeight = 400;
+      initialBottom = 280;
+      break;
+    case "ipad":
+      jumpHeight = 550;
+      initialBottom = 390;
+      break;
+    default:
+      jumpHeight = 550;
+      initialBottom = 380;
+  }
   let currentLeft = dog.style.left;
   dog.style.transition = "bottom 0.3s ease-out";
-  dog.style.bottom = "550px";
-
+  dog.style.bottom = `${jumpHeight}px`;
   setTimeout(() => {
     dog.style.transition = "bottom 0.3s ease-in";
-    dog.style.bottom = "400px";
+    dog.style.bottom = `${initialBottom}px`;
     dog.style.left = currentLeft;
-
     setTimeout(() => {
       dog.style.display = "none";
       isJumping = false;
@@ -57,16 +95,35 @@ function jump() {
 /**************************DOG LAUGH***************************** */
 
 function dogLaugh() {
-  const dog = document.getElementById("dog");
-  let isLaughing = false;
+  dog.style.backgroundPosition = "-240px -58px";
 
-  // Set initial position
+  const deviceType = getDeviceType();
+  let initialBottom, laughJumpHeight, finalJumpHeight;
+
+  switch (deviceType) {
+    case "mobile":
+      initialBottom = 280;
+      laughJumpHeight = 100;
+      finalJumpHeight = 40;
+      break;
+    case "ipad":
+      initialBottom = 390;
+      laughJumpHeight = 150;
+      finalJumpHeight = 60;
+      break;
+    default:
+      initialBottom = 298;
+      laughJumpHeight = 100;
+      finalJumpHeight = 60;
+  }
+
+  adjustAnimationForDevice();
+  let isLaughing = false;
   dog.style.display = "block";
   dog.style.left = "40%";
-  dog.style.bottom = "290px";
   dog.style.zIndex = "100";
+  dog.style.bottom = `${initialBottom}px`;
 
-  // Function to change laugh sprite
   function toggleLaughSprite() {
     if (isLaughing) {
       dog.style.backgroundPosition = "-240px -58px";
@@ -76,57 +133,68 @@ function dogLaugh() {
     isLaughing = !isLaughing;
   }
 
-  // Start laughing animation
   const laughInterval = setInterval(toggleLaughSprite, 240);
 
-  // Move dog up
   setTimeout(() => {
     dog.style.transition = "bottom 2s ease-out";
-    dog.style.bottom = "400px";
+    dog.style.bottom = `${initialBottom + laughJumpHeight}px`;
   }, 10);
 
-  // Move dog down
   setTimeout(() => {
-    dog.style.transition = "bottom 2s ease-in";
-    dog.style.bottom = "260px";
+    dog.style.transition = "bottom 1s ease-in";
+    dog.style.bottom = `${initialBottom + laughJumpHeight - finalJumpHeight}px`;
   }, 4000);
 
-  // Stop animation and hide dog
   setTimeout(() => {
     clearInterval(laughInterval);
     dog.style.display = "none";
-  }, 5500);
+  }, 5000);
 }
 
 /**************************CATCH ONE BIRD***************************** */
 
 function dogCatchBird() {
+  const deviceType = getDeviceType();
+  let jumpHeight, initialBottom;
+
+  switch (deviceType) {
+    case "mobile":
+      jumpHeight = 100;
+      initialBottom = 280;
+      break;
+    case "ipad":
+      jumpHeight = 100;
+      initialBottom = 310;
+      break;
+    default:
+      jumpHeight = 100;
+      initialBottom = 300;
+  }
+
+  adjustAnimationForDevice();
   dog.style.display = "block";
-  // Initial sprite
   dog.style.backgroundPosition = "-319px 0px";
-  dog.style.left = "800px";
-  dog.style.bottom = "300px";
+  dog.style.left = "50%";
   dog.style.zIndex = "-100";
+  dog.style.bottom = `${initialBottom}px`;
   dog.offsetHeight;
-  // Start rising animation
+
   setTimeout(() => {
-    dog.style.bottom = "400px";
+    dog.style.bottom = `${initialBottom + jumpHeight}px`;
     dog.style.transition = "bottom 1s ease 0.5s";
   }, 1000);
-  // Hold at the top
+
   setTimeout(() => {
     dog.style.backgroundPosition = "-319px 0px";
   }, 1000);
-  // Start descending slowly
+
   setTimeout(() => {
-    dog.style.bottom = "300px";
-  }, 3000); // Time to stay on top
-  //Reset position
+    dog.style.bottom = `${initialBottom}px`;
+  }, 3000);
+
   setTimeout(() => {
     dog.style.display = "none";
   }, 4000);
 }
 
-//dogCatchBird();
-//startDogAnimation();
 dogLaugh();
